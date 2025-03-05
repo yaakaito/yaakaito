1ed2ef8b8a78b41d664f1353a78efa7197bacb46
yaakaito <yaakaito@gmail.com>
2025-03-05 11:23:24 +0000

    fix #48

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import remarkOgpCard from './scripts/remark-ogp-card.js';
+import remarkCollapsibleCode from './scripts/remark-collapsible-code.js';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://yaakai.to',
     markdown: {
         remarkPlugins: [
-            remarkOgpCard
+            remarkOgpCard,
+            remarkCollapsibleCode
         ]
     }
 });
